8a76f41403e5dfa9ea5aa69b847863fa9626113c
Bogdan Cuza <bogdancuza@protonmail.com>
2022-06-13 19:43:37 +0300

    commit tree e0aab272118ef3966ada82a4b9d0fb07b001c2b2

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,18 @@
-# Write your code here
+require 'net/http'
+require 'open-uri'
+require 'json'
+
+class GetRequester
+    def initialize(url)
+        uri = URI.parse(URL)
+        @response = Net::HTTP.get_response(uri)
+    end
+
+    def get_response_body
+        @response.body
+    end
+
+    def parse_json
+        JSON.parse(self.get_response_body)
+    end
+end
